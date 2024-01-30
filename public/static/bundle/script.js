08f3891be21c7e0e3a05391b878e11c085d3ddfb
SurapaneniVarshini <varshinisurapaneni@yahoo.com>
2024-01-30 17:03:06 -0600

    first commit

---
+++ public/static/bundle/script.js
@@ -0,0 +1,33 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.ts":
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+/***/ (() => {
+
+eval("\nvar GetMovieService = /** @class */ (function () {\n    function GetMovieService() {\n        this.apiKey = \"0dc79e9f8d8261756060e27eae2708db\";\n        this.baseUrl = \"https://api.themoviedb.org/3/search/movie?api_key=\";\n        this.mainUrl = this.baseUrl + this.apiKey + \"&query=\";\n        this.url = '';\n    }\n    GetMovieService.prototype.getMovieDetails = function (searchInput) {\n        this.url = this.mainUrl + searchInput;\n        return fetch(this.mainUrl + searchInput)\n            .then(function (response) { return response.json(); })\n            .then(function (data) { return data; })\n            .catch(function (error) {\n            console.error(\"Error: \".concat(error.message));\n            throw error;\n        });\n    };\n    GetMovieService.prototype.getPaginatedData = function (pageNumber) {\n        return fetch(this.url + \"&page=\" + pageNumber)\n            .then(function (response) { return response.json(); })\n            .then(function (data) { return data; })\n            .catch(function (error) {\n            console.error(\"Error: \".concat(error.message));\n            throw error;\n        });\n    };\n    return GetMovieService;\n}());\nvar SearchComponent = /** @class */ (function () {\n    function SearchComponent(movieService) {\n        this.movieService = movieService;\n        this.userInput = document.getElementById('movieInput');\n        this.resultsDiv = document.getElementById('results');\n        this.pageDetails = { page: 1, results: [], total_pages: 0, total_results: 0 };\n        this.pageSize = this.pageDetails.results.length;\n        this.currentPage = 1;\n        this.totalPages = this.pageDetails.total_pages;\n        this.pageData = [];\n    }\n    SearchComponent.prototype.init = function () {\n        var _this = this;\n        var _a, _b;\n        this.userInput.addEventListener('input', debounce(function () {\n            var input = _this.userInput.value;\n            _this.movieService.getMovieDetails(input)\n                .then(function (response) {\n                _this.pageDetails = response;\n                _this.pageSize = _this.pageDetails.results.length;\n                _this.totalPages = _this.pageDetails.total_pages;\n                _this.pageData = _this.pageDetails.results;\n                _this.renderResults();\n            })\n                .catch(function (error) {\n                console.error(\"Error: \".concat(error.message));\n            });\n        }, 750));\n        (_a = document.getElementById('nextBtn')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function () { return _this.onNextPage(); });\n        (_b = document.getElementById('prevBtn')) === null || _b === void 0 ? void 0 : _b.addEventListener('click', function () { return _this.onPreviousPage(); });\n    };\n    SearchComponent.prototype.onNextPage = function () {\n        if (this.currentPage < this.totalPages) {\n            this.currentPage += 1;\n            this.getPaginatedData();\n        }\n    };\n    SearchComponent.prototype.onPreviousPage = function () {\n        if (this.currentPage > 1 && this.currentPage <= this.totalPages) {\n            this.currentPage -= 1;\n            this.getPaginatedData();\n        }\n    };\n    SearchComponent.prototype.getPaginatedData = function () {\n        var _this = this;\n        this.movieService.getPaginatedData(this.currentPage)\n            .then(function (response) {\n            _this.pageDetails = response;\n            _this.pageSize = _this.pageDetails.results.length;\n            _this.totalPages = _this.pageDetails.total_pages;\n            _this.pageData = _this.pageDetails.results;\n            _this.renderResults();\n        })\n            .catch(function (error) {\n            console.error(\"Error: \".concat(error.message));\n        });\n    };\n    SearchComponent.prototype.renderResults = function () {\n        var _this = this;\n        this.resultsDiv.innerHTML = '';\n        var pageNumberElement = document.getElementById('pageNumber');\n        if (pageNumberElement) {\n            pageNumberElement.textContent = \"Page \".concat(this.currentPage, \" / \").concat(this.totalPages);\n        }\n        this.pageData.forEach(function (movie) {\n            var movieElement = document.createElement('div');\n            movieElement.classList.add('movie');\n            movieElement.innerHTML = \"\\n          <h3>\".concat(movie.title, \"</h3>\\n          <p>\").concat(movie.overview, \"</p>\\n          <p>Release Date: \").concat(movie.release_date, \"</p>\\n          <p>Vote Average: \").concat(movie.vote_average, \"</p>\\n          <img src=\\\"https://image.tmdb.org/t/p/w500\").concat(movie.poster_path, \"\\\" alt=\\\"\").concat(movie.title, \"\\\">\\n        \");\n            _this.resultsDiv.appendChild(movieElement);\n        });\n    };\n    return SearchComponent;\n}());\nfunction debounce(func, delay) {\n    var timeoutId;\n    return function () {\n        var args = arguments;\n        clearTimeout(timeoutId);\n        timeoutId = setTimeout(function () { return func.apply(null, args); }, delay);\n    };\n}\nvar movieService = new GetMovieService();\nvar searchComponent = new SearchComponent(movieService);\nsearchComponent.init();\n\n\n//# sourceURL=webpack://tmdb/./src/index.ts?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.ts"]();
+/******/ 	
+/******/ })()
+;
